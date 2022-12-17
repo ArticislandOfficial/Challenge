0d30285aa3c4c19bbf606374337dbc7892bc7793
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-12-16 20:57:55 -0600

    2.Conectando componentes

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Contenido from '../components/Contenido'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+const HomePage = () => {
+  return (
+
+    <div>
+        <Header/>
+        <Contenido/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default HomePage
